99d664122a401e9c1011a9fd5a85471ecce87ee8
Yifang Zhu <70597936+zhu-yifang@users.noreply.github.com>
2023-10-14 20:50:33 +0800

    update views.py

--- blog/views.py
+++ blog/views.py
@@ -1,20 +1,9 @@
 from django.shortcuts import render
-
-posts = [{
-    'author': 'CoreyMS',
-    'title': 'Blog Post 1',
-    'content': 'First post content',
-    'date_posted': 'August 27, 2021'
-}, {
-    'author': 'Jane Doe',
-    'title': 'Blog Post 2',
-    'content': 'Second post content',
-    'date_posted': 'August 28, 2021'
-}]
+from .models import Post
 
 
 def home(request):
-    context = {'posts': posts}
+    context = {'posts': Post.objects.all()}
     return render(request, 'blog/home.html', context)
 
 
