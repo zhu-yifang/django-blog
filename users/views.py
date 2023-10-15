0c49f3370eb22d3009b91ee38e6f491b6914576a
Yifang Zhu <70597936+zhu-yifang@users.noreply.github.com>
2023-10-15 18:52:46 +0800

    Created profile function

--- users/views.py
+++ users/views.py
@@ -9,8 +9,14 @@ def register(request):
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
-            messages.success(request, f'Your account has been created! You are now able to log in')
+            messages.success(
+                request,
+                f'Your account has been created! You are now able to log in')
             return redirect('login')
     else:
         form = UserRegisterForm()
     return render(request, 'users/register.html', {'form': form})
+
+
+def profile(request):
+    return render(request, 'users/profile.html')
