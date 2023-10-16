5767390bb342aa5863b4d67360ec4f5d642db88f
Yifang Zhu <70597936+zhu-yifang@users.noreply.github.com>
2023-10-16 20:52:04 +0800

    update 3 files and create 1 file

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, CreateView
 from .models import Post
 
 
@@ -17,6 +17,11 @@ class PostListView(ListView):
 
 class PostDetailView(DetailView):
     model = Post
+
+
+class PostCreateView(CreateView):
+    model = Post
+    fields = ['title', 'content']
 
 
 def about(request):
