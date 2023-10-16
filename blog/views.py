82ed8567b0c4af53e182eaf3c9739954334b2c7a
Yifang Zhu <70597936+zhu-yifang@users.noreply.github.com>
2023-10-16 21:05:10 +0800

    update views.py

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.views.generic import ListView, DetailView, CreateView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView
 from .models import Post
 
 
@@ -25,6 +25,16 @@ class PostCreateView(LoginRequiredMixin, CreateView):
     fields = ['title', 'content']
 
     def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+
+class PostUpdateView(LoginRequiredMixin, UpdateView):
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        # set the author of the post to the current user
         form.instance.author = self.request.user
         return super().form_valid(form)
 
