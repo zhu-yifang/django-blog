0dfd94846d206fd485cbe41974948fe8e593d5e1
Yifang Zhu <70597936+zhu-yifang@users.noreply.github.com>
2023-10-18 18:53:22 +0800

    update views.py

--- blog/views.py
+++ blog/views.py
@@ -14,6 +14,7 @@ class PostListView(ListView):
     template_name = 'blog/home.html'  # <app>/<model>_<viewtype>.html
     context_object_name = 'posts'  # default is object_list
     ordering = ['-date_posted']  # order posts from newest to oldest
+    paginate_by = 2  # number of posts per page
 
 
 class PostDetailView(DetailView):
