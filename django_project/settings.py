bc60e7e7d7d42175f999416377a03fd522d86980
Yifang Zhu <70597936+zhu-yifang@users.noreply.github.com>
2024-01-09 23:32:52 -0800

    Update DEFAULT_FILE_STORAGE in settings.py

--- django_project/settings.py
+++ django_project/settings.py
@@ -149,4 +149,4 @@ AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
 AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = None
 
-DEFAULT_FILE_STORAGE = 'storage.backends.s3boto3.S3Boto3Storage'
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
